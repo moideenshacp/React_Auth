ec71e36690aa23ed774ae596e31aa9d229504b92
moideenshacp <153590227+moideenshacp@users.noreply.github.com>
2024-09-13 16:26:13 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import { error } from 'console';
+dotenv.config()
 
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDb');
+}).catch((error)=>{
+    console.log(error);
+    
+})
 const app = express()
 
 app.listen(3000,()=>{
