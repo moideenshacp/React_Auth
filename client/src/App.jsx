c500422ce8d4d9145468353765124fdc35041fec
moideenshacp <153590227+moideenshacp@users.noreply.github.com>
2024-09-13 16:08:49 +0530

    create and run server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-
+import { BrowserRouter,Route,Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Header from "./components/Header"
 const App = () => {
   return (
-    <div className="text-blue-600">App</div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/profile" element={<Profile/>} />
+      <Route path="/sign-in" element={<SignIn/>} />
+      <Route path="/sign-up" element={<SignUp/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
